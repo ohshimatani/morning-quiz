b3102cdbe737ada4249a975cb721daeb4804ae0c
ohshimatani <ohshimatani0326@gmail.com>
2022-06-06 19:44:41 +0900

    added start scene

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React from 'react';
+import StartScene from "./components/StartScene";
 
 function App() {
   return (
-    <div>
-			<h1 className="text-3xl font-bold text-center m-5">
-				H1 Title
-			</h1>
-		</div>
+    // <div>
+		// 	<h1 className="text-3xl font-bold text-center m-5">
+		// 		H1 Titleeee
+		// 	</h1>
+		// </div>
+		<StartScene />
   );
 }
 
