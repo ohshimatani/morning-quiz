2affe2f39394817ec6efc3e05950e6d214802fd4
ohshimatani <ohshimatani0326@gmail.com>
2022-06-07 08:04:28 +0900

    corrected start page UI

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -2,11 +2,17 @@ function StartButton() {
 	const thisClassName = `
 		text-4xl
 		text-center
+		font-thin
 		bg-blue-400
 		p-8
 		rounded-xl
 		w-1/6
-		hover:text-blue-50`;
+		mx-auto
+		shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]
+		hover:text-blue-50
+		hover:shadow-none
+		hover:opacity-90
+		`;
 	return (
 		<div className={thisClassName}>スタート</div>
 	);
--- src/components/StartTitle.jsx
+++ src/components/StartTitle.jsx
@@ -1,8 +1,8 @@
 function StartTitle () {
 	return (
-		<div className="font-bold">
-			<h1 className="text-5xl text-center">忘れた頃にやってくる！IT略語クイズ！</h1>
-			<h2 className="text-4xl text-center">〜気持ちいい朝を迎えよう〜</h2>
+		<div className="font-thin">
+			<h1 className="text-5xl text-center m-10">忘れた頃にやってくる！IT略語クイズ！</h1>
+			<h2 className="text-4xl text-center m-8">〜気持ちいい朝を迎えよう〜</h2>
 		</div>
 	);
 };
