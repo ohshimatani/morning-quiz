0df273520316f210522cc2a14fa58c25640987b8
ohshimatani <ohshimatani0326@gmail.com>
2022-06-08 19:47:30 +0900

    fixed a JSON file

--- src/components/WordClassLabel.jsx
+++ src/components/WordClassLabel.jsx
@@ -1,12 +1,18 @@
 const WordClassLabel = (props) => {
 	return (
-		<p className={pClassName}>{props.classLabel}</p>
+		<p className={pClassName}>category: {props.classLabel}</p>
 	);
 }
 
 const pClassName = `
-	text-l
-	text-thin
+	text-3xl
+	font-thin
+	text-right
+	w-5/6
+	-mt-16
+	mb-10
+	pr-10
+	underline
 `;
 
 export default WordClassLabel;
